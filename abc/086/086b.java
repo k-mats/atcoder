9ba6973a4b0f30b3eeb3129168669efa8b475652
k-mats <gfeenhaftsylphid@gmail.com>
2018-05-10 00:45:27 +0900

    Add 086b

---
+++ abc/086/086b.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String a = sc.next();
+		String b = sc.next();
+		int c = Integer.parseInt(a + b);
+		for (int i = 1; i * i <= c; i++) {
+			if (i * i == c) {
+				System.out.println("Yes");
+				return;
+			}
+		}
+		System.out.println("No");
+  }
+}
